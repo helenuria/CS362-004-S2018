206174e8d564d85109d518e3655e0fbddd25dc9e
Helen Stockman <helen.n.stockman@gmail.com>
2018-05-31 09:53:04 -0700

    manual tests

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -6,22 +6,69 @@ import junit.framework.TestCase;
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
 // Again, it is up to you to use this file or not!
 
-
-
-
-
 public class UrlValidatorTest extends TestCase {
 
 
-   public UrlValidatorTest(String testName) {
-      super(testName);
+   public void UrlValidatorTest(boolean retVal, boolean expectVal) {
+      if (retVal == expectVal) {
+		  System.out.print("Test Passed\n");
+	  } else {
+		  System.out.print("Test Failed\n");
+	  }
    }
-
-   
    
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
+	   //You can use this function to implement your manual testing	   
+	   UrlValidator urlVal = new UrlValidator();
+
+	   System.out.print("\nScheme is http, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("http://facebook.com"), true);
+	   
+	   System.out.print("\nScheme is ftp, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("ftp://facebook.com"), true);
+
+	   System.out.print("\nScheme is incorrect, expect false:\n");
+	   UrlValidatorTest(urlVal.isValid("http//facebook.com"), false);
+
+	   System.out.print("\nNo scheme, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("facebook.com"), true);
+
+	   System.out.print("\nAuthority is valid, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com"), true);
+
+	   System.out.print("\nAuthority is invalid, expect false:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foo.bar.com"), false);
+	   
+	   System.out.print("\nNo authority, expect false:\n");
+	   UrlValidatorTest(urlVal.isValid("www..com"), false);
+
+	   System.out.print("\nPort is valid, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com:80"), true);
+
+	   System.out.print("\nPort is invalid, expect false:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com:-5"), false);
+	   
+	   System.out.print("\nNo Port, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com"), true);
+
+	   System.out.print("\nPath is valid, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com/test"), true);
+
+	   System.out.print("\nPath is invalid, expect false:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com/test//test"), false);
+
+	   System.out.print("\nNo Path, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com"), true);
+
+	   System.out.print("\nQuery is valid, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com?test=true"), true);
+
+	   System.out.print("\nQuery is invalid, expect false:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com??query"), false);
+	   
+	   System.out.print("\nNo query, expect true:\n");
+	   UrlValidatorTest(urlVal.isValid("www.foobar.com"), true);
 	   
    }
    
